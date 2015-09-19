d935a0e6cbc6e0d3d5f3f1b8818bdb270508e14c
Clifton McIntosh <cliff.mcintosh@gmail.com>
2015-09-19 11:51:39 -0500

    Initial commit with just gradle and a main application.

---
+++ src/main/java/com/cliftonmcintosh/creditcard/Application.java
@@ -0,0 +1,17 @@
+package com.cliftonmcintosh.creditcard;
+
+
+public class Application  {
+
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.run(args);
+    }
+
+    public Application() {
+    }
+
+    public void run(String... args) {
+    }
+}
