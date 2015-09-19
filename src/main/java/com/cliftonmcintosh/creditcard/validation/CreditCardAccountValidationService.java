3756b8742d320ca98dfae68d2b3c997f2317da30
Clifton McIntosh <cliff.mcintosh@gmail.com>
2015-09-19 16:40:56 -0500

    small improvements to validation of account creation -- chain predicates with and, create constant for maximum length for an account number.

--- src/main/java/com/cliftonmcintosh/creditcard/validation/CreditCardAccountValidationService.java
+++ src/main/java/com/cliftonmcintosh/creditcard/validation/CreditCardAccountValidationService.java
@@ -13,6 +13,8 @@ public class CreditCardAccountValidationService implements AccountValidationServ
 
     private static Pattern LIMIT_FORMAT_PATTERN = Pattern.compile("^\\$[0-9]*");
 
+    private static int MAXIMUM_ACCOUNT_NUMBER_LENGTH = 19;
+
     public boolean validateAccountCreationRequest(String name, String accountNumber, String limit) {
         return name != null && !name.trim().isEmpty() && ACCOUNT_NUMBER_CHECK.test(accountNumber) && LIMIT_CHECK.test(limit);
     }
@@ -27,12 +29,14 @@ public class CreditCardAccountValidationService implements AccountValidationServ
         return false;
     }
 
+    private static Predicate<String> ACCOUNT_NUMBER_LENGTH_CHECK = (String input) -> input != null && input.length() <= MAXIMUM_ACCOUNT_NUMBER_LENGTH;
+
     private static Predicate<String> IS_DIGITS_CHECK = (String input) -> {
         Matcher matcher = DIGITS_PATTERN.matcher(input);
         return matcher.matches();
     };
 
-    private static Predicate<String> ACCOUNT_NUMBER_CHECK = (String input) -> input.length() < 20 && IS_DIGITS_CHECK.test(input) && LuhnTest.INSTANCE.test(input);
+    private static Predicate<String> ACCOUNT_NUMBER_CHECK = (String input) -> ACCOUNT_NUMBER_LENGTH_CHECK.and(IS_DIGITS_CHECK).and(LuhnTest.INSTANCE::test).test(input);
 
     private static Predicate<String> LIMIT_CHECK = (String input) -> {
         Matcher matcher = LIMIT_FORMAT_PATTERN.matcher(input);
