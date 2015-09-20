88cb0d62418cf4f27c7fb67ccfe83a3243edfe06
Clifton McIntosh <cliff.mcintosh@gmail.com>
2015-09-20 06:33:31 -0500

    Add a processor for handling input string.

---
+++ src/main/java/com/cliftonmcintosh/creditcard/input/InputProcessor.java
@@ -0,0 +1,7 @@
+package com.cliftonmcintosh.creditcard.input;
+
+/**
+ * Created by cmcintosh on 9/20/15.
+ */
+public interface InputProcessor {
+}
---
+++ src/test/groovy/com/cliftonmcintosh/creditcard/SpaceDelimitedInputProcessorSpec.groovy
@@ -0,0 +1,11 @@
+package com.cliftonmcintosh.creditcard
+
+import spock.lang.Specification
+
+
+/**
+ * Created by cmcintosh on 9/20/15.
+ */
+class SpaceDelimitedInputProcessorSpec extends Specification {
+
+}
